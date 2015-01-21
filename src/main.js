f4ba5107a369a385e329f15320b77f385a946ba9
Petr Sloup <slouppetr@gmail.com>
2015-01-21 10:51:20 +0100

    Added nominatim (+ matcher) implementation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 goog.provide('kt.decorate');
 
+goog.require('kt.Nominatim');
 goog.require('kt.alert');
 goog.require('kt.expose');
 
@@ -9,6 +10,10 @@ goog.require('kt.expose');
  */
 kt.decorate = function() {
   //TODO: decorate controls when it makes sense
+  goog.array.forEach(goog.dom.getElementsByClass('autocomplete-nominatim'),
+      function(el, i, arr) {
+        new kt.Nominatim(el);
+      });
 };
 
 kt.expose.symbol('kt.decorate', kt.decorate);
